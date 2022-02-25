c980f7db03133f18ffff74b0c8da4ca346ef402c
Noah Reed <Nore5515@gmail.com>
2022-02-25 17:58:25 -0600

    init

---
+++ Assets/Topdown2DPitfall/DemoScene/DemoScripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using Nevelson.Topdown2DPitfall.Assets.Scripts.Utils;
+using UnityEngine;
+
+public class Player : MonoBehaviour, IPitfallCheck, IPitfallObject {
+    public float speed = 7f;
+    private Rigidbody2D rb;
+    private bool isMovable = true;
+    private Vector2 moveVelocity;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        if (!isMovable) {
+            MovementInput(Vector2.zero);
+            return;
+        }
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        MovementInput(input);
+    }
+
+    void FixedUpdate() {
+        rb.MovePosition(rb.position + moveVelocity * Time.fixedDeltaTime);
+    }
+
+    private void MovementInput(Vector2 moveInput) {
+        moveVelocity = moveInput.normalized * speed;
+    }
+
+    public bool PitfallConditionCheck() {
+        return true;
+    }
+
+    public void PitfallActionsBefore() {
+        isMovable = false;
+    }
+
+    public void PitfallResultingAfter() {
+        isMovable = true;
+    }
+}
---
+++ Assets/Topdown2DPitfall/Scripts/Utils/UtilCoroutines.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Nevelson.Topdown2DPitfall.Assets.Scripts.Utils {
+    public static class UtilCoroutines {
+        public static IEnumerator FallingCo(GameObject fallingObj, Action ActionsAfterPitfall,
+            float pitfallAnimSpeed, Vector2 respawnLocation) {
+            Vector2 scaleReduction = new Vector2(pitfallAnimSpeed, pitfallAnimSpeed);
+            while (fallingObj.transform.GetLocalScale2D().x > 0) {
+                fallingObj.transform.SetLocalScale2D(fallingObj.transform.GetLocalScale2D() - scaleReduction);
+                yield return null;
+            }
+            yield return new WaitForSecondsRealtime(.1f);
+            ActionsAfterPitfall();
+            fallingObj.transform.SetPosition2D(respawnLocation);
+            yield return new WaitForSecondsRealtime(.1f);
+            fallingObj.transform.SetLocalScale2D(Vector2.one);
+        }
+    }
+}
