b1e1d95694b8876487f9117638a59e019fe5ec28
Noah Reed <Nore5515@gmail.com>
2022-02-25 20:01:01 -0600

    added enemy script

---
+++ Assets/Mouse.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+using UnityEngine.UIElements;
+
+public class Mouse : MonoBehaviour
+{
+
+    public Tilemap world;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Vector3Int mousePos = GetMousePosition();
+
+        // Left mouse click -> add path tile
+        if (Input.GetMouseButton(0))
+        {
+            world.SetTile(mousePos, null);
+        }
+    }
+
+    // :)
+    Vector3Int GetMousePosition()
+    {
+        Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        return world.WorldToCell(mouseWorldPos);
+    }
+}
