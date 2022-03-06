213f9db65d4d1560e167817f6796f9c0c95d1899
Noah Reed <Nore5515@gmail.com>
2022-03-06 12:45:19 -0600

    granted upon thee enemies HP

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,15 +2,6 @@
 
 public class Enemy : MonoBehaviour
 {
-    public float speed = 4f;
-    private Rigidbody2D rb;
-    private bool isMovable = true;
-    private Vector2 moveVelocity;
+    public float HP = 10;
 
-    // public GameObject dest;
-
-    void Start()
-    {
-        rb = GetComponent<Rigidbody2D>();
-    }
 }
