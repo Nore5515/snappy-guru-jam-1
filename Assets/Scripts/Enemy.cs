90501d31198e3b6a225ef3b46ac54dc3b508dedf
Noah Reed <Nore5515@gmail.com>
2022-03-06 12:50:36 -0600

    granted traps damage and teh ability to deal it

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,6 +2,14 @@
 
 public class Enemy : MonoBehaviour
 {
-    public float HP = 10;
+    public int HP = 10;
 
+    public void setHP(int _HP)
+    {
+        HP = _HP;
+    }
+    public int getHP()
+    {
+        return HP;
+    }
 }
